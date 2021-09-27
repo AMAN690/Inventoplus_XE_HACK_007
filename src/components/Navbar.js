ee509459f847bf120d142846f30517955bb2ecb1
Piyush1105 <41717967+Piyush1105@users.noreply.github.com>
2021-09-27 13:41:23 +0530

    increased style  clearity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,6 +18,9 @@ function Navbar() {
           <Link to="#" className="menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
+          <h1 className="navbar-heading">
+            Xebia Inventoplus_XE_HACK_007 Xethon
+          </h1>
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>

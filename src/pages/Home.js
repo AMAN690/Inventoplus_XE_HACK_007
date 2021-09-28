8fb2ad095c2401cfb64059762862e606d6cfd787
Piyush1105 <41717967+Piyush1105@users.noreply.github.com>
2021-09-28 11:27:33 +0530

    charts added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./Home.css";
+import Chart1 from "../charts/chart1";
 
 function Home() {
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div className="home">
+      <h1>CHARTS</h1>
+      <div className="container">
+        <div className="item item-1">
+          <Chart1 />
+        </div>
+        <div className="item item-2">Chart2</div>
+        <div className="item item-3">Chart3</div>
+        <div className="item item-4">Chart4</div>
+        <div className="item item-5">Chart5</div>
+        <div className="item item-6">Chart6</div>
+      </div>
     </div>
   );
 }
